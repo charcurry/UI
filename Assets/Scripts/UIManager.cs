7503dc3ef0684d72757c959acb2b6f33f8ae6ff6
Charlie <charlieryancurry@gmail.com>
2024-02-27 09:56:28 -0400

    add showhide function and random & exit buttons

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,15 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+using Random = System.Random;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    public Slider red;
+    public Slider green;
+    public Slider blue;
+    public Slider alpha;
+
+
+    public TextMeshProUGUI showHideButtonText;
+    public GameObject coin;
+    public Image colorChangeImage;
+
+    bool show = false;
+
     public int score;
     public TextMeshProUGUI scoreText;
 
     public void Awake()
     {
         scoreText.text = "Score: 0";
+    }
+
+    public void Update()
+    {
+        ColorChange();
     }
 
     public void ScoreUpdate(int num)
@@ -23,7 +44,44 @@ public class UIManager : MonoBehaviour
         {
             score += num;
         }
-        score = Mathf.Clamp(score, -999999999, 999999999);
+        score = Mathf.Clamp(score, 0, 999999999);
         scoreText.text = "Score: " + score.ToString("n0");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Random()
+    {
+        Random rnd = new();
+        int num = rnd.Next(-999999999, 999999999);
+        if (num == 0)
+        {
+            num = 1;
+        }
+        ScoreUpdate(num);
+    }
+
+    public void ColorChange()
+    {
+        colorChangeImage.color = new Color(red.value, green.value, blue.value, alpha.value);
+    }
+
+    public void ShowHide()
+    {
+        show = !show;
+
+        if (show == true)
+        {
+            showHideButtonText.text = "Hide";
+            coin.SetActive(true);
+        }
+        else if (show == false)
+        {
+            showHideButtonText.text = "Show";
+            coin.SetActive(false);
+        }
     }
 }
